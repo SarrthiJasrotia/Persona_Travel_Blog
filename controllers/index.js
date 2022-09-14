fef9285732c5bc932c4265d5508d134fc50dc5d7
Sarrthi Jasrotia <sarrthijasrotia05@gmail.com>
2022-09-13 19:31:48 -0500

    made 2 diffrent views folders

--- controllers/index.js
+++ controllers/index.js
@@ -8,14 +8,14 @@ const Posts = require('../models/blogPost')
 //Index
 blogRouter.get('/', (req, res) => {
     Posts.find({}, (error, blogPosts) => {
-        res.render('index.ejs', {
+        res.render('home/index.ejs', {
             postsIndex: blogPosts,
         });
     });
 });
 //New
 blogRouter.get("/new",(req,res)=>{
-    res.render('new.ejs')
+    res.render('home/new.ejs')
 });
 //Delete
 blogRouter.delete('/:id',(req,res)=>{
@@ -23,32 +23,36 @@ blogRouter.delete('/:id',(req,res)=>{
         res.redirect('/blogPost')
     });
 });
+
 //Update
 blogRouter.put('/:id/',(req,res)=>{
     Posts.findByIdAndUpdate(req.params.id, req.body,()=>{
         res.redirect(`/posts/${req.params.id}`)
     });
 });
+
 //Create
 blogRouter.post("/",(req,res)=>{
     Posts.create(req.body, (error, createdPost)=>{
         res.redirect('/blogPost')
     });
 });
+
 //Edit
 blogRouter.get('/:id/edit',(req,res)=>{
     Posts.findById(req.params.id, (error,blogPosts)=>{
-        res.render('edit.ejs',{
+        res.render('home/edit.ejs',{
             postsEdit:blogPosts
         });
     });
 });
+
 //Show
 blogRouter.get('/:id',(req,res)=>{
     Posts.findById(req.params.id,(error,blogPosts)=>{
-        res.render('show.ejs',{
+        res.render('home/show.ejs',{
             postsShow:blogPosts,
         });
-    });  
+    });
 });
 module.exports = blogRouter
